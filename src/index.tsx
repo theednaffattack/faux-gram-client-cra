9d11d214e41eef3b79d319c650292c03b4613427
eddie naff <eddienaff@eddienaff.com>
2019-07-23 16:31:48 -0700

    Test environment vars to set connections

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { ApolloClient } from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
 import { ThemeProvider } from "styled-components";
-import cookie from "cookie";
+// import cookie from "cookie";
 
 import { setContext } from "apollo-link-context";
 import { createUploadLink } from "apollo-upload-client";
@@ -22,20 +22,55 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { navigate } from "@reach/router";
 
-const domain = `fauxgramapi.eddienaff.dev`; // process.env.NODE_ENV; // === "production" ? prodDomain : myLanInfo;
+// const domain = [
+//   { production: `fauxgramapi.eddienaff.dev`, development: `localhost:4000` }
+// ];
 
-const prodGraphqlUrl = `https://${domain}/graphql`;
-
-const prodWebsocketsUrl = `wss://${domain}/subscriptions`;
-
-function parseCookies(req?: any, options = {}) {
-  return cookie.parse(
-    req && req.headers ? req.headers.cookie || "" : document.cookie,
-    options
-  );
+interface IEnvironmentConfig {
+  ssl: boolean;
+  port: number | null;
+  hostname: string;
+  httpProtocol: string[];
 }
 
-const getToken = (req: Request) => parseCookies(req).qid;
+interface IConfigurations {
+  [key: string]: IEnvironmentConfig;
+}
+
+const configurations: IConfigurations = {
+  // Note: You may need sudo to run on port 443
+  production: {
+    httpProtocol: ["https", "wss"],
+    ssl: true,
+    port: null,
+    hostname: "fauxgramapi.eddienaff.dev"
+  },
+  development: {
+    httpProtocol: ["http", "ws"],
+    ssl: false,
+    port: 4000,
+    hostname: "192.168.1.10:4000"
+  }
+};
+
+const environment = (process.env.NODE_ENV as string) || "production";
+
+const prodGraphqlUrl = `${configurations[environment].httpProtocol[0]}://${
+  configurations[environment].hostname
+}/graphql`;
+
+const prodWebsocketsUrl = `${configurations[environment].httpProtocol[1]}://${
+  configurations[environment].hostname
+}/subscriptions`;
+
+// function parseCookies(req?: any, options = {}) {
+//   return cookie.parse(
+//     req && req.headers ? req.headers.cookie || "" : document.cookie,
+//     options
+//   );
+// }
+
+// const getToken = (req: Request) => parseCookies(req).qid;
 
 const uploadLink = createUploadLink({
   // uri: `http://${myIpAddress}:4000/graphql`,
@@ -70,6 +105,7 @@ const splitLink = split(
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
+    // @ts-ignore
     graphQLErrors.map(({ message, locations, path }) => {
       console.log(
         `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(
@@ -81,6 +117,11 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
       if (message.includes("Not authenticated")) {
         navigate("/login");
       }
+      return `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(
+        locations,
+        null,
+        2
+      )}, Path: ${path}`;
     });
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
