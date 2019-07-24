033f85091df7f56fe5fb5cb2d9bde99d3abb0334
eddie naff <eddienaff@eddienaff.com>
2019-07-23 19:57:28 -0700

    Adjust messages page rendering to allow loader

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 
 import { MeComponent, HelloWorldComponent } from "../generated/graphql";
-// import ViewThreads from "../components/view-threads";
+import ViewThreads from "../components/view-threads";
 import { IPageProps } from "./types";
+
+// function StrData(args: any) {
+//   return <pre>{JSON.stringify(args, null, 2)}</pre>;
+// }
 
 const Messages: React.FC<IPageProps> = ({ path }: IPageProps) => {
   return (
     <HelloWorldComponent>
       {helloData => (
         <MeComponent>
-          {(data: any) => {
-            if (!data && !data.data.me) {
-              return <div>loading....</div>;
-            } else {
-              return <div>{JSON.stringify(data.data, null, 2)}</div>; // <ViewThreads me={data.data.me.id} data={data} />;
+          {({ data, loading, error }) => {
+            if (loading) return <div>loading...</div>;
+            if (error) return <div>error: {error}</div>;
+            if (data && data.me) {
+              return <ViewThreads me={data.me.id} data={data} />; // <StrData args={data} />; //
             }
           }}
         </MeComponent>
