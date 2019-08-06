1668b3093088859c74e09ee7bae920287af61faa
eddie naff <eddienaff@eddienaff.com>
2019-08-05 22:04:52 -0700

    Add posed comp for auth errors

--- src/components/posed-components.tsx
+++ src/components/posed-components.tsx
@@ -12,3 +12,18 @@ export const PButton = posed(Button)({
   enter: { x: 0, opacity: 1 },
   exit: { x: 50, opacity: 0 }
 });
+
+export const PosedFlash = posed(Flex)({
+  enter: { y: 0, opacity: 1, delay: 500 },
+  exit: { y: 20, opacity: 0 }
+});
+
+export const Content = posed(Flex)({
+  closed: { height: 0, opacity: 0 },
+  open: { height: "auto", opacity: 1 }
+});
+
+export const MessageContainer = posed(Flex)({
+  open: { height: "auto", opacity: 1, delayChildren: 200, staggerChildren: 50 },
+  closed: { height: 0, opacity: 0, delayChildren: 200, staggerChildren: 50 }
+});
