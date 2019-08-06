6a8ed7c9bcd509e41fa377d2d5fb888cad9dcd40
eddie naff <eddienaff@eddienaff.com>
2019-08-05 22:08:09 -0700

    Add wrap to profile image

--- src/components/user-profile-image.tsx
+++ src/components/user-profile-image.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-import { Button, Flex, Icon, Text, FlexShadow } from "./styled-rebass";
+import {
+  Button,
+  Flex,
+  Icon,
+  Text,
+  FlexShadow,
+  FlexUserProfileWrap
+} from "./styled-rebass";
 import { IUserProfileImage } from "./types";
 
 function UserProfileImage({
@@ -21,7 +28,8 @@ function UserProfileImage({
       flexDirection={flexInstruction ? flexInstruction : "row"}
       alignItems="center"
     >
-      <FlexShadow
+      {/* <FlexShadow
+        className="flex-shadow"
         height="40px"
         width="40px"
         my={2}
@@ -31,9 +39,21 @@ function UserProfileImage({
         justifyContent="center"
         boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
         style={{ borderRadius: "50%", overflow: "hidden" }}
+      > */}
+
+      <FlexUserProfileWrap
+        maxHeight="40px"
+        width="40px"
+        overflow="hidden"
+        borderRadius="50%"
+        bg="thread_footer"
+        alignItems="center"
+        justifyContent="center"
+        boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
       >
         <Icon mt={3} size="2em" name="user" fill="white" />
-      </FlexShadow>
+      </FlexUserProfileWrap>
+      {/* </FlexShadow> */}
       <Text color={color ? color : "text"}>
         {user.firstName} {user.lastName}
       </Text>
