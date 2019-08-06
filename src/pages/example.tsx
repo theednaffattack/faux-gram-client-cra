8f2a4842f426ea87900e119a2cdb1ae95c9a2dd8
eddie naff <eddienaff@eddienaff.com>
2019-08-05 22:17:15 -0700

    Add framer motion example

---
+++ src/pages/example.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { useRef } from "react";
+import { motion, useCycle } from "framer-motion";
+import { useDimensions } from "../hooks/use-dimensions";
+import { MenuToggle } from "../components/menu-toggle";
+import { Navigation } from "../components/navigation";
+import { IPageProps } from "./types";
+// import "./styles.css";
+
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2
+    }
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 40px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40
+    }
+  }
+};
+
+export const Example = ({ path }: IPageProps) => {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { height } = useDimensions(containerRef);
+
+  return (
+    <motion.nav
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+      custom={height}
+      ref={containerRef}
+    >
+      <motion.div className="background" variants={sidebar} />
+      <Navigation />
+      <MenuToggle toggle={() => toggleOpen()} />
+    </motion.nav>
+  );
+};
