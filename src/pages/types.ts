ad293d79313194c00971db9953e1ea2623a91359
eddie naff <eddienaff@eddienaff.com>
2019-08-05 22:22:17 -0700

    Add to page props to prevent reach router focus bugs

--- src/pages/types.ts
+++ src/pages/types.ts
@@ -2,6 +2,9 @@ import { ApolloClient, NormalizedCacheObject } from "apollo-boost";
 
 export interface IPageProps {
   path: string;
+  state?: any;
+  primary?: boolean;
+  navigate?: any;
 }
 
 export interface MyContext {
