b88cdc8d33e4fd8fac5090efa9b9164d14c40917
eddie naff <eddienaff@eddienaff.com>
2019-08-16 01:23:13 -0700

    Add functions that may be unneeded due to react-window

--- src/components/mounted-proof.tsx
+++ src/components/mounted-proof.tsx
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 interface IMountedProofProps {
   handleUpdateMessageState: any;
+  handleSetLastMessage: any;
+  handleSetLastMessenger: any;
+  lastMessage: string;
+  lastMessenger: any;
+  lastMessageValue: string;
+  lastMessengerValue: any;
 }
 
 export default class MountedProof extends Component<
@@ -10,8 +16,16 @@ export default class MountedProof extends Component<
 > {
   componentDidMount() {
     this.props.handleUpdateMessageState();
+    this.props.handleSetLastMessage();
+    this.props.handleSetLastMessenger(this.props.lastMessenger);
   }
   render() {
-    return <div> .</div>;
+    return (
+      <div>
+        {" "}
+        .{this.props.lastMessenger.firstName}
+        {this.props.lastMessage}
+      </div>
+    );
   }
 }
