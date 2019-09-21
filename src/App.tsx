854b0c7022f3dc6f337ca20839d01e7092a8f8a7
eddie naff <eddienaff@eddienaff.com>
2019-09-21 11:37:24 -0700

    Clone elements for navigation menu controls

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,87 @@ import Layout from "./components/layout";
 
 const App = () => (
   <Layout>
-    <Flex flexDirection="column" id="site-container" flex="1 1 auto" width={1}>
-      <GlobalStyle />
+    <GetFromClonedLayoutChildren>
       <Flex
-        flexDirection="row"
-        id="content-container"
+        flexDirection="column"
+        id="site-container"
         flex="1 1 auto"
         width={1}
       >
-        <Routes />
+        <GlobalStyle />
+        <Flex
+          flexDirection="row"
+          key="target"
+          id="content-container"
+          flex="1 1 auto"
+          width={1}
+        >
+          <Routes key="my-router" />
+        </Flex>
       </Flex>
-    </Flex>
+    </GetFromClonedLayoutChildren>
   </Layout>
 );
 
 export default App;
+
+const GetFromClonedLayoutChildren = (props: any) => {
+  const children = React.Children.map(props.children, (child: any) => {
+    let newElement = React.cloneElement(child, {
+      showMessagingAddressBook: props.showMessagingAddressBook,
+      handleCreateNewMessageThread: props.handleCreateNewMessageThread,
+      handleCancelNewMessageThread: props.handleCancelNewMessageThread,
+      ...props
+    });
+
+    let { children: nestedChildren } = newElement.props.children.props;
+
+    let findMyRouter = nestedChildren.map((child: any) => {
+      if (child.key === "target" && child.props.children.key === "my-router") {
+        let newRouterChild = React.cloneElement(child.props.children, {
+          showMessagingAddressBook: props.showMessagingAddressBook,
+
+          handleCreateNewMessageThread: props.handleCreateNewMessageThread,
+          handleCancelNewMessageThread: props.handleCancelNewMessageThread,
+          handleLoadNewThreadCreated: props.handleLoadNewThreadCreated
+        });
+
+        let tryToClone = Object.assign({}, child, {
+          props: { ...child.props, children: newRouterChild }
+        });
+
+        return tryToClone;
+      }
+      return child;
+    });
+
+    let newerElement = Object.assign({}, newElement, {
+      props: {
+        ...newElement.props,
+        children: [...findMyRouter]
+      }
+    });
+
+    return newerElement;
+  });
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%"
+      }}
+    >
+      <div
+        style={{
+          flex: "1 1 auto",
+          display: "flex",
+          flexDirection: "column",
+          width: "100%"
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
