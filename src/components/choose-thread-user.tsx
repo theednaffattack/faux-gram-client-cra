46f7a0a13449e8fabd49374518e9851241cd164c
eddie naff <eddienaff@eddienaff.com>
2019-09-21 11:42:59 -0700

    Add functionality to address book chooser

--- src/components/choose-thread-user.tsx
+++ src/components/choose-thread-user.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import { Button, Heading, Text, Flex } from "./styled-rebass";
 
@@ -8,11 +8,13 @@ export interface IChooseThreadUserState {
 }
 
 export interface IChooseThreadUserProps {
-  messages: any[];
+  messages?: any[];
   dataCreateThread: any;
+  newThreadInvitees: any[];
   loadingCreateThread: any;
   errorCreateThread: any;
   handleAddInviteeToThread: any;
+  handleRemoveInviteeToThread: any;
 }
 
 export class ChooseThreadUser extends React.Component<
@@ -31,14 +33,19 @@ export class ChooseThreadUser extends React.Component<
       dataCreateThread,
       loadingCreateThread,
       errorCreateThread,
-      handleAddInviteeToThread
+      handleAddInviteeToThread,
+      handleRemoveInviteeToThread,
+      newThreadInvitees
     } = this.props;
 
+    const filteredListOfThoseICanMessage = dataCreateThread.filter(
+      (user: any) => newThreadInvitees.indexOf(user) < 0
+    );
     return (
-      <Fragment>
+      <Flex alignItems="center" width={[1, 1, 1]} flexDirection="column">
         <Heading
-          as="h3"
-          color="chat_icon"
+          as="h2"
+          // color="chat_icon"
           fontWeight={200}
           p={3}
           mr="auto"
@@ -47,38 +54,108 @@ export class ChooseThreadUser extends React.Component<
               open: !open
             })
           }
-        />
-        <Flex width={[1, 1, 1]} flexDirection="column">
-          {loadingCreateThread ? <Text>not loading</Text> : ""}
-          {errorCreateThread ? <Text>no error</Text> : ""}
-          {dataCreateThread ? (
-            dataCreateThread.map((person: any, index: number) => (
-              <Flex
-                border="2px #b2b2d8 solid"
-                key={`select-msg-receiver_${index}`}
-                alignItems="center"
-                width={["300px", "300px", "300px"]}
-                p={2}
-                my={1}
-                bg="white"
-              >
-                <Text fontSize="1.8em" width={[2 / 3, 2 / 3, 2 / 3]} mr="auto">
-                  {person.firstName} {person.lastName}
-                </Text>
-                <Button
-                  onClick={() => handleAddInviteeToThread({ user: person })}
-                  type="button"
-                  bg="#736eab"
-                >
-                  +
-                </Button>
-              </Flex>
-            ))
-          ) : (
-            <Text>no data</Text>
-          )}
+        >
+          Invite Users
+        </Heading>
+        <Flex width={[1, 1, 1, 1, 2 / 3]}>
+          <Flex alignItems="center" width={[1, 1, 1]} flexDirection="column">
+            <Heading
+              as="h2"
+              // color="chat_icon"
+              fontWeight={200}
+              p={3}
+              onClick={() =>
+                this.setState({
+                  open: !open
+                })
+              }
+            >
+              Invited
+            </Heading>
+            {newThreadInvitees
+              ? newThreadInvitees.map((person: any, index: number) => (
+                  <Flex
+                    border="2px #b2b2d8 solid"
+                    key={`potential_receiver_${index}`}
+                    alignItems="center"
+                    width={["300px", "300px", "300px"]}
+                    p={2}
+                    my={1}
+                    bg="white"
+                  >
+                    <Text
+                      fontSize="1.8em"
+                      width={[2 / 3, 2 / 3, 2 / 3]}
+                      mr="auto"
+                    >
+                      {person.firstName} {person.lastName}
+                    </Text>
+                    <Button
+                      onClick={() => {
+                        handleRemoveInviteeToThread({ user: person });
+                      }}
+                      type="button"
+                      bg="crimson"
+                      // bg="#736eab"
+                    >
+                      X
+                    </Button>
+                  </Flex>
+                ))
+              : "none"}
+          </Flex>
+          <Flex alignItems="center" width={[1, 1, 1]} flexDirection="column">
+            <Heading
+              as="h2"
+              // color="chat_icon"
+              fontWeight={200}
+              p={3}
+              onClick={() =>
+                this.setState({
+                  open: !open
+                })
+              }
+            >
+              Not Invited
+            </Heading>
+            {loadingCreateThread ? <Text>not loading</Text> : ""}
+            {errorCreateThread ? <Text>no error</Text> : ""}
+
+            {dataCreateThread ? (
+              filteredListOfThoseICanMessage.map(
+                (person: any, index: number) => (
+                  <Flex
+                    border="2px #b2b2d8 solid"
+                    key={`select-msg-receiver_${index}`}
+                    alignItems="center"
+                    width={["300px", "300px", "300px"]}
+                    p={2}
+                    my={1}
+                    bg="white"
+                  >
+                    <Text
+                      fontSize="1.8em"
+                      width={[2 / 3, 2 / 3, 2 / 3]}
+                      mr="auto"
+                    >
+                      {person.firstName} {person.lastName}
+                    </Text>
+                    <Button
+                      onClick={() => handleAddInviteeToThread({ user: person })}
+                      type="button"
+                      bg="#736eab"
+                    >
+                      +
+                    </Button>
+                  </Flex>
+                )
+              )
+            ) : (
+              <Text>no data</Text>
+            )}
+          </Flex>
         </Flex>
-      </Fragment>
+      </Flex>
     );
   }
 }
